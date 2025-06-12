0da4a792777ec6a7c51450ed38998f7eba76854d
Phil Splettstoeszer <stoeszer09@gmail.com>
2025-06-12 14:21:03 -0500

    register letter press

--- js/main.js
+++ js/main.js
@@ -2,9 +2,9 @@ const ALPHABET = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P
 const TOP_ROW_LETTERS = ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"];
 const MIDDLE_ROW_LETTERS = ["A", "S", "D", "F", "G", "H", "J", "K", "L"];
 const BOTTOM_ROW_LETTERS = ["Z", "X", "C", "V", "B", "N", "M"];
-const ROTOR_I = { type: "I",}
-const ROTOR_II = { type: "II",}
-const ROTOR_III = { type: "III",}
+const ROTOR_I = { type: "I", wiring: {A: "E", B: "K", C: "M", D: "F", E: "L", F: "G", G: "D", H: "Q", I: "V", J: "Z", K: "N", L: "T", M: "O", N: "W", O: "Y", P: "H", Q: "X", R: "U", S: "S", T: "P", U: "A", V: "I", W: "B", X: "R", Y: "C", Z: "J"}}
+const ROTOR_II = { type: "II", wiring: {A: "A", B: "J", C: "D", D: "K", E: "S", F: "I", G: "R", H: "U", I: "X", J: "B", K: "L", L: "H", M: "W", N: "T", O: "M", P: "C", Q: "Q", R: "G", S: "Z", T: "N", U: "P", V: "Y", W: "F", X: "V", Y: "O", Z: "E"}}
+const ROTOR_III = { type: "III", wiring: {  A: "B", B: "D", C: "F", D: "H", E: "J", F: "L", G: "C", H: "P", I: "R", J: "T", K: "X", L: "V", M: "Z", N: "N", O: "Y", P: "E", Q: "I", R: "W", S: "G", T: "A", U: "K", V: "M", W: "U", X: "S", Y: "Q", Z: "O"}}
 
 const lightPanel = document.querySelector("#light-panel")
 const topRowContainer = document.querySelector("#top-row-lights")
@@ -53,3 +53,28 @@ function displaySingleRotor(rotorSlotElement, rotorData, slotIndex) {
 displaySingleRotor(rotorSlot1, activeRotors[0], 0)
 displaySingleRotor(rotorSlot2, activeRotors[1], 1)
 displaySingleRotor(rotorSlot3, activeRotors[2], 2)
+
+document.addEventListener("keydown", e => {
+  const pressedKey = e.key.toUpperCase()
+  if (ALPHABET.includes(pressedKey)) {
+    e.preventDefault()
+    const lightElementID = `light-${pressedKey}`
+    const lightElement = document.getElementById(lightElementID)
+    
+    if (lightElement) {
+      lightElement.classList.add("active")
+    }
+  }
+})
+document.addEventListener("keyup", e => {
+  e.preventDefault()
+  const releasedKey = e.key.toUpperCase()
+  if (ALPHABET.includes(releasedKey)) {
+    const lightElementID = `light-${releasedKey}`
+    const lightElement = document.getElementById(lightElementID)
+    
+    if (lightElement) {
+      lightElement.classList.remove("active")
+    }
+  }
+})
